55adb5ff601eebb97280de4f8652a2b822086f69
Sérgio Vicente Tavuencas <sergiovicente_tavuenc@outlook.com>
2022-11-03 21:27:55 -0300

    Novas validações e comentários

--- src/test/java/Conta.java
+++ src/test/java/Conta.java
@@ -3,57 +3,86 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class Conta {
-	double saldo;
-	double saque;
+	Integer saldo;
+	Integer saque;
 	boolean clienteEspecial;
 	
+	/**
+	 * 
+	 *	@param saldo representa o valor total na conta do cliente.
+	 *	@implNote o método verifica se o valor passado para o parametro é igual a -200.
+	 */
 	@Given("um cliente atual com o saldo de {int} reais")
-	public void um_cliente_atual_com_o_saldo_de_reais(Integer int1) {
-	    this.saldo = int1;
+	public void um_cliente_atual_com_o_saldo_de_reais(Integer saldo) {
+	    this.saldo = saldo;
 	    
-	    if (this.saldo != int1) {
+	    if (this.saldo != -200) {
 	    	throw new io.cucumber.java.PendingException();
 	    }
 	}
-
+	
+	/**
+	 * 
+	 *	@param saque representa o valor que será retirado.
+	 *	@implNote o método verifica se o valor passado para o parametro é igual a 100.
+	 */
 	@When("for solicitado um saque no valor de {int} reais")
-	public void for_solicitado_um_saque_no_valor_de_reais(Integer int1) {
-	    this.saque = int1;
+	public void for_solicitado_um_saque_no_valor_de_reais(Integer saque) {
+	    this.saque = saque;
 	    
-	    if (this.saque != int1) {
+	    if (this.saque != 100) {
 	    	throw new io.cucumber.java.PendingException();
 	    }
 	}
 
+	/**
+	 * 
+	 *	@param saldo representa o valor total na conta do cliente.
+	 *	@implNote o método verifica se o valor passado para o parametro é igual a -300.
+	 */
 	@Then("deve efetuar o saque e atualizar o  saldo da conta para {int} reais")
-	public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(Integer int1) { 
+	public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(Integer saldo) { 
 		this.clienteEspecial = true;
-	    if ((this.saldo -= this.saque) == int1 && this.clienteEspecial) {
-	    	this.saldo = int1;
+	    if ((this.saldo -= this.saque) == -300 && this.clienteEspecial) {
+	    	this.saldo = saldo;
 	    	this.saque = 0;
 	    } else {
 	    	throw new io.cucumber.java.PendingException();
 	    }
 	}
-
+	
+	/**
+	 * 
+	 *	@param saldo representa o valor total na conta do cliente.
+	 *	@implNote o método verifica se o valor passado para o parametro é igual a -300.
+	 */
 	@Given("um cliente comum com saldo atual de {int} reais")
-	public void um_cliente_comum_com_saldo_atual_de_reais(Integer int1) {
-	    this.saldo = int1;
+	public void um_cliente_comum_com_saldo_atual_de_reais(Integer saldo) {
+	    this.saldo = saldo;
 	    
-	    if (this.saldo != int1) {
+	    if (this.saldo != -300) {
 	    	throw new io.cucumber.java.PendingException();
 	    }
 	}
 
+	/**
+	 * 
+	 *	@param saque representa o valor que será retirado.
+	 *	@implNote o método verifica se o valor passado para o parametro é igual a -200.
+	 */
 	@When("solicitar um saque de {int} reais")
-	public void solicitar_um_saque_de_reais(Integer int1) {
-		this.saque = int1;
+	public void solicitar_um_saque_de_reais(Integer saque) {
+		this.saque = saque;
 	    
-	    if (this.saque != int1) {
+	    if (this.saque != -200) {
 	    	throw new io.cucumber.java.PendingException();
 	    }
 	}
-
+	
+	/**
+	 * 
+	 *	@implNote o método verifica se o saldo é maior que zero e se é cliente especial.
+	 */
 	@Then("não deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente")
 	public void não_deve_efetuar_o_saque_e_deve_retornar_a_mensagem_saldo_insuficiente() {
 		this.clienteEspecial = false;
